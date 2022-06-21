3c6f476f88e8ef105fd94252bf8d18ef747ddd66
Vasco Jonas Mabui <vascojonas.j@gmail.com>
2022-06-21 02:01:03 +0200

    Calculo de IRPS

--- app/Http/Livewire/MaoObra.php
+++ app/Http/Livewire/MaoObra.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use Error;
+use Exception;
 use Livewire\Component;
 
 class MaoObra extends Component
@@ -9,14 +11,15 @@ class MaoObra extends Component
 
     public $salarioBruto;
     public $salarioLiquido;
-    public $inss;
+    public $inss=0.07;
     public $irps;
-    public $nrDependentes;
+    public $nrDependentes=0;
 
     public $valorMinimo;
     public $coefiiente;
+    public $valorDependentes=0;
 
-
+    public $campo;
   
   
     public function salarioLiquido(){
@@ -24,36 +27,121 @@ class MaoObra extends Component
         if($this->salarioBruto >=20250 && $this->salarioBruto<20750){
                 $this->valorMinimo=20250;
                 $this->coefiiente=0.10;
+                
         }else if($this->salarioBruto >=20750 && $this->salarioBruto<21000){
                 $this->valorMinimo=20750;
                 $this->coefiiente=0.10;
+                if($this->nrDependentes==0){
+                    $this->valorDependentes=50;
+                }
         }else if($this->salarioBruto >=21000 && $this->salarioBruto<21250){
             $this->valorMinimo=21000;
             $this->coefiiente=0.10;
+            if($this->nrDependentes==0){
+                $this->valorDependentes=75;
+            }else if($this->nrDependentes==1){
+                $this->valorDependentes=25;
+            }
         }else if($this->salarioBruto >=21250 && $this->salarioBruto<21750){
             $this->valorMinimo=21250;
             $this->coefiiente=0.10;
+            if($this->nrDependentes==0){
+                $this->valorDependentes=100;
+            }else if($this->nrDependentes==1){
+                $this->valorDependentes=50;
+            }else if($this->nrDependentes==2){
+                $this->valorDependentes=25;
+            }
         }else if($this->salarioBruto >=21750 && $this->salarioBruto<22250){
             $this->valorMinimo=21750;
             $this->coefiiente=0.10;
+            if($this->nrDependentes==0){
+                $this->valorDependentes=150;
+            }else if($this->nrDependentes==1){
+                $this->valorDependentes=100;
+            }else if($this->nrDependentes==2){
+                $this->valorDependentes=75;
+            }else if($this->nrDependentes==3){
+                $this->valorDependentes=50;
+            }
         }else if($this->salarioBruto >=22250 && $this->salarioBruto<32750){
             $this->valorMinimo=22250;
             $this->coefiiente=0.15;
+            if($this->nrDependentes==0){
+                $this->valorDependentes=200;
+            }else if($this->nrDependentes==1){
+                $this->valorDependentes=150;
+            }else if($this->nrDependentes==2){
+                $this->valorDependentes=125;
+            }else if($this->nrDependentes==3){
+                $this->valorDependentes=100;
+            }if($this->nrDependentes>=4){
+                $this->valorDependentes=50;
+            }
         }else if($this->salarioBruto >=32750 && $this->salarioBruto<60750){
             $this->valorMinimo=32750;
             $this->coefiiente=0.20;
+            if($this->nrDependentes==0){
+                $this->valorDependentes=1775;
+            }else if($this->nrDependentes==1){
+                $this->valorDependentes=1725;
+            }else if($this->nrDependentes==2){
+                $this->valorDependentes=1700;
+            }else if($this->nrDependentes==3){
+                $this->valorDependentes=1675;
+            }if($this->nrDependentes>=4){
+                $this->valorDependentes=1625;
+            }
         }else if($this->salarioBruto >=60750 && $this->salarioBruto<144750){
             $this->valorMinimo=60750;
             $this->coefiiente=0.25;
+            if($this->nrDependentes==0){
+                $this->valorDependentes=7375;
+            }else if($this->nrDependentes==1){
+                $this->valorDependentes=7325;
+            }else if($this->nrDependentes==2){
+                $this->valorDependentes=7300;
+            }else if($this->nrDependentes==3){
+                $this->valorDependentes=7275;
+            }if($this->nrDependentes>=4){
+                $this->valorDependentes=7225;
+            }
         }else if($this->salarioBruto >=144750 ){
             $this->valorMinimo=144750;
             $this->coefiiente=0.32;
+            if($this->nrDependentes==0){
+                $this->valorDependentes=28375;
+            }else if($this->nrDependentes==1){
+                $this->valorDependentes=28325;
+            }else if($this->nrDependentes==2){
+                $this->valorDependentes=28300;
+            }else if($this->nrDependentes==3){
+                $this->valorDependentes=28275;
+            }if($this->nrDependentes>=4){
+                $this->valorDependentes=28225;
+            }
         }else{
             $this->valorMinimo=0;
-            $this->coefiiente=1;   
+            $this->coefiiente=0;   
+            $this->valorDependentes=0;
         }
+
+        try {
+            $this->irps= ($this->salarioBruto /($this->salarioBruto-(($this->salarioBruto-$this->valorMinimo)
+            *$this->coefiiente + $this->valorDependentes)))-1;
+        } 
+        catch (Error $th) {
+            $this->irps=0;
+        }
+        
+
+         $this->irps =round($this->irps,5);            
     }
 
+
+    public function habilitar($id){
+       $this->campo=$id;
+    }
 
 
 
--- resources/views/livewire/mao-obra.blade.php
+++ resources/views/livewire/mao-obra.blade.php
@@ -22,76 +22,86 @@
             <tbody>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">INSS</th>
-                <td class="pb-1 pt-1 mb-1 mt-1" ></td>
-                <td class="pb-1 pt-1 mb-1 mt-1" ></td>
-                <td class="pb-1 pt-1 mb-1 mt-1" ></td>
+                <td class="p-0" ><input wire:click="habilitar(1)" wire:model="inss" readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
               </tr>
               <tr>
-                <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">IPRS</th>
-                <td class="pb-1 pt-1 mb-1 mt-1" ></td>
-                <td class="pb-1 pt-1 mb-1 mt-1" ></td>
-                <td class="pb-1 pt-1 mb-1 mt-1" ></td>
+                <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">IRPS</th>
+                <td class="p-0" ><input wire:click="habilitar(4)" wire:model="irps" class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">Férias anuais </th>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
     
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">Repousos semanais remunerado </th>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">Faltas justificadas  </th>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">Feriados </th>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">Feriado por dia da cidade </th>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row"> 13º salário</th>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
               </tr>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">Total Parcial</th>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               <tr>
-                <th scope="row">Incidência Cumulativa (Grupo A sobre Grupo B)</th>
-                <td> </td>
-                <td> </td>
-                <td> </td>
+                <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">Incidência Cumulativa (A sobre B)</th>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1"  scope="row">Total dos Encargos</th>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
-                <td class="pb-1 pt-1 mb-1 mt-1" > </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               <tr>
                 <th class="pb-1 pt-1 mb-1 mt-1" scope="row">Percentagem</th>
-                <td class="pb-1 pt-1 mb-1 mt-1"> </td>
-                <td class="pb-1 pt-1 mb-1 mt-1"> </td>
-                <td class="pb-1 pt-1 mb-1 mt-1"> </td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file m-0 " type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+                <td class="p-0" ><input readonly class="bg-light form-control-file" type="number" /></td>
+
               </tr>
               
             </tbody>
@@ -99,13 +109,15 @@
     </div>
 
     <div class=" p-0 ml-auto" style="width: 38%">
-        <div class="col-12 p-0">
-            <div class="input-group mb-3">
+      <div class="col-12 p-0">
+        
+      @if ($campo===4)
+        <div class="input-group mb-3">
                 <div class="input-group-prepend">
                   <span class="input-group-text" id="basic-addon1">Salário Bruto</span>
                 </div>
                 <input type="number" class="form-control" placeholder="Digite o  sálario Bruto" aria-label="salario"
-                 aria-describedby="basic-addon1 " wire:model="salarioBruto">
+                aria-describedby="basic-addon1 " wire:model="salarioBruto"  wire:click="salarioLiquido">
               </div>
     
               <div class="input-group mb-3">
@@ -113,12 +125,21 @@
                     <span class="input-group-text" id="basic-addon1">Dependentes</span>
                   </div>
                   <input type="number" class="form-control" placeholder="Digite o número de dependentes" 
-                  aria-label="salario" aria-describedby="basic-addon1 wire:model="nrDependentes"">
-                </div>
+                  aria-label="salario" aria-describedby="basic-addon1" wire:model="nrDependentes"  wire:click="salarioLiquido">
+            </div>
     
         </div>
+          
+        <div class="text-right">
+            <button wire:click="salarioLiquido" class="btn btn-secondary col-4 ml-auto">Calcular</button>
+        </div>
+      @else
+          
+      @endif
 
-        <table class="table table-striped table-bordered table-secondary mt-3">
+
+
+      <!--  <table class="table table-striped table-bordered table-secondary mt-3">
             <tr>
               <th class="pb-1 pt-1 mb-1 mt-1 w-50">Vencimento Mensal (VM)</th>
               <td class="pb-1 pt-1 mb-1 mt-1"> </td>
@@ -142,10 +163,7 @@
             </tr>
         </table>
 
-        <div class="text-right">
-            <button class="btn btn-secondary col-4 ml-auto">Calcular</button>
-        </div>
-    </div>
+     </div> -->
 
    
 
