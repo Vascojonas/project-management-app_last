dea6a399f29a5863e70763ff8e7249538ad078c7
Vasco Jonas Mabui <vascojonas.j@gmail.com>
2022-06-28 18:53:01 +0200

    css on prodution

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+   
     /**
      * Bootstrap any application services.
      *
@@ -24,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      //
+        
     }
 }
